3f51d8fe33ae02a8adb73ddb7c0ce5a3fb23399a
Aryan Tomar <aryantomar239@gmail.com>
2025-03-07 01:19:21 +0530

    changes are made

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
 import HeroSection from '@/app/components/HeroSection';
-import StoreSection from '@/app/components/StoreSection';
+
 import RashiSection from '@/app/components/RashiSection';
 import ConsultingSection from '@/app/components/ConsultingSection';
 import StatsBar from '@/app/components/StatsBar';
 import BlogSection from '@/app/components/BlogSection';
 import AppDownload from '@/app/components/AppDownload';
 import WhyConsult from '@/app/components/WhyConsult';
+import AstrologerCarousel from '@/app/components/ConsultAstrologer';
 
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <StoreSection />
-      <RashiSection />
+      <AstrologerCarousel />
       <ConsultingSection />
       <StatsBar />
       <BlogSection />
+      <RashiSection />
       <AppDownload />
       <WhyConsult />
      
